6f188ae76a6118c7963773ea7e25a580309787bf
zack.zhang <zackzhang@augmentum.com.cn>
2022-09-08 16:32:29 +0800

    action: add sonar check

--- backend/graphql/graphql-test/src/main/java/io/github/alice52/graphql/GraphqlApplication.java
+++ backend/graphql/graphql-test/src/main/java/io/github/alice52/graphql/GraphqlApplication.java
@@ -1,8 +1,8 @@
 package io.github.alice52.graphql;
 
+import common.core.util.spring.UniqueNameGenerator;
 import common.redis.config.ExcludeRedisConfig;
 import common.swagger.annotation.EnableSwagger;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Import;
  * @project graphql <br>
  */
 @EnableSwagger
-@Import(ExcludeRedisConfig.class)
+@Import({ExcludeRedisConfig.class, UniqueNameGenerator.class})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RabbitAutoConfiguration.class})
 public class GraphqlApplication {
     public static void main(String[] args) {
